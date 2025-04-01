569e338bffa20bd9d26c5772db27b90006f39663
lducoding <lducoding@gmail.com>
2025-04-01 21:45:21 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/com/musinsa/problem/cody/CodyApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.problem.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodyApplication.class, args);
+	}
+
+}
